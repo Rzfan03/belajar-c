abb9becd1df049dd02f5d9c219b0e3c0a189bdbe
rzfann <fajrulsyaban47@gmail.com>
2026-02-14 15:15:17 +0700

    update day 2

---
+++ project/digital_clock.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+int main() {
+    time_t rawtime = 0;
+    struct tm *pTime = NULL;
+    bool isRunning = true;
+    
+    
+    printf("simple digital clock\n");
+    
+    while (isRunning) {
+        time(&rawtime);
+        pTime = localtime(&rawtime);
+        printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+        sleep(1);
+    }
+
+    
+    
+    return 0;
+}
